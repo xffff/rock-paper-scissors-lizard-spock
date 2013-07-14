186d716e22ed5d6b70c1fd7aa90691a8a1e3f6fb
xffff <shouhei@gmail.com>
2013-07-14 18:21:26 +0100

    prefer this indentation

--- moves.h
+++ moves.h
@@ -4,12 +4,12 @@
 class Moves {
  public:
   Moves()
-    {
-      m.push_back(new Rock("Rock"));
-      m.push_back(new Paper("Paper"));
-      m.push_back(new Scissors("Scissors"));
-      m.push_back(new Lizard("Lizard"));
-      m.push_back(new Spock("Spock"));
-    }
+  {
+    m.push_back(new Rock("Rock"));
+    m.push_back(new Paper("Paper"));
+    m.push_back(new Scissors("Scissors"));
+    m.push_back(new Lizard("Lizard"));
+    m.push_back(new Spock("Spock"));
+  }
   std::vector<Element*> m;
 };
